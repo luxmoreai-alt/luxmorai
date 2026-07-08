fb76bd7b75a13ee82e41f98cb0244d89aec93f86
tazvirotechnologies-max <tazvirotechnologies@gmail.com>
2026-07-08 11:51:33 +0530

    Changes updated

--- backend/api/index.py
+++ backend/api/index.py
@@ -1,12 +1,4 @@
 import os
-import sys
-from pathlib import Path
-
-BASE_DIR = Path(__file__).resolve().parent.parent
-BACKEND_DIR = BASE_DIR / "backend"
-
-if str(BACKEND_DIR) not in sys.path:
-    sys.path.insert(0, str(BACKEND_DIR))
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "luxbackend.settings")
 
